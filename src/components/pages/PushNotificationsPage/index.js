51535ce08d709f1a651adc6b03856991dcbad0a8
nguyentruc011099 <100280570+nguyentruc011099@users.noreply.github.com>
2022-06-04 15:07:26 +0700

    Design UI

--- src/components/pages/PushNotificationsPage/index.js
+++ src/components/pages/PushNotificationsPage/index.js
@@ -1,8 +1,33 @@
+import {
+  Typography,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@mui/material";
+import { pushNotificationsFields } from "constant/data";
 import { useState, useEffect } from "react";
-import './style.scss';
+import "./style.scss";
 
 const PushNotificationsPage = () => {
-  return <></>;
+  return (
+    <Typography component="div" className="push-noti-container">
+      {pushNotificationsFields.map((item) => (
+        <FormControl className="field-checkbox-container">
+          <FormLabel id={item.field}>{item.title}</FormLabel>
+          <RadioGroup
+            aria-labelledby={item.field}
+            defaultValue="off"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="on" control={<Radio />} label="On" />
+            <FormControlLabel value="off" control={<Radio />} label="Off" />
+          </RadioGroup>
+        </FormControl>
+      ))}
+    </Typography>
+  );
 };
 
 export default PushNotificationsPage;
--- src/components/pages/SettingPage/index.js
+++ src/components/pages/SettingPage/index.js
@@ -1,9 +1,15 @@
-import  React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { settingOption } from "constant/data";
+import EditProfilePage from "components/pages/EditProfilePage";
+import "./style.scss";
+import PushNotificationsPage from "../PushNotificationsPage";
+import EmailNotificationsPage from "../EmailNotificationsPage";
+import ChangePasswordPage from "../ChangePasswordPage";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -34,7 +40,7 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
@@ -46,22 +52,33 @@ export default function SettingPage() {
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+    <Box sx={{ width: "100%" }} className="user-setting-container">
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          sx={{ borderRight: 1, borderColor: "divider" }}
+          className="tabs-option-container"
+        >
+          {settingOption.map((item, index) => {
+            return <Tab label={item.label} {...a11yProps(index)} />;
+          })}
         </Tabs>
       </Box>
-      <TabPanel value={value} index={0}>
-        Item One
+      <TabPanel value={value} index={0} className="setting-content">
+        <EditProfilePage />
       </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
+      <TabPanel value={value} index={1} className="setting-content">
+        <ChangePasswordPage />
       </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
+      <TabPanel value={value} index={2} className="setting-content">
+        <EmailNotificationsPage />
+      </TabPanel>
+      <TabPanel value={value} index={3} className="setting-content">
+        <PushNotificationsPage />
       </TabPanel>
     </Box>
   );
